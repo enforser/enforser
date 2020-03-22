1c917a2b92c3596be795bce3e6562b45967fd30d
enforser <trailcapital@gmail.com>
2020-03-22 19:09:16 -0400

    fix languag

--- src/blog/pages/index.clj
+++ src/blog/pages/index.clj
@@ -9,7 +9,7 @@
     [:p
      "Welcome! My name is Matthew Fors."]
     [:p
-     "I'm a software engineer currently located in NYC. I moved to New York in May of 2019 with my then girlfriend and dog from Ottawa, Canada. While in Ottawa, I attended Carleton University from 2014 through to 2019 to complete a Bachelors degree in computer science, with a minor in philosophy."]
+     "I'm a software engineer currently located in NYC. I moved to New York in May of 2019 with my girlfriend and dog from Ottawa, Canada. While in Ottawa, I attended Carleton University from 2014 through to 2019 to complete a Bachelors degree in computer science, with a minor in philosophy."]
     [:p
      "In my spare time, you may find me exploring new vegan restaurants, watching reality TV, or hanging out at a punk show. My philosophical interests include consequentialist ethics and philosophy of mind."]
     [:p
