6d12ba1e70f2e718402ce9b653f559c511b33fc1
enforser <trailcapital@gmail.com>
2020-04-24 23:09:27 -0400

    spelling error

---
+++ src/blog/pages/posts/repling_in_scala.clj
@@ -0,0 +1,9 @@
+(ns enforser.pages.posts.repling-in-scala)
+
+(def title "Clojure to Scala")
+
+(defn page
+  [context]
+  [:div {:class "container"}
+   [:div {:class "row"}
+    ]])
---
+++ src/blog/pages/posts/site_structure.clj
@@ -0,0 +1,10 @@
+(ns blog.pages.posts.site-structure)
+
+(defn content
+  [context]
+  [[:p ""]])
+
+(def details
+  {:date "22-03-2020"
+   :title "Site Structure: Static Site with Hiccup"
+   :content content})
